f1b5e38cb642c4f85c375838c587cb350e1bdcae
Kavin Manikandan <88193949+LunarFang13@users.noreply.github.com>
2023-04-01 02:59:48 +0530

    sa

--- server.js
+++ server.js
@@ -12,13 +12,13 @@ const ngrok = require('ngrok');
 
 app.use(
     cors({
-      origin: ["http://localhost:3000", "http://localhost/"],
+      origin: ["http://localhost:3000", "http://localhost"],
       methods: ["GET", "POST"],
       credentials: true,
     })
   );
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost/');
+  res.header('Access-Control-Allow-Origin', 'http://localhost');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   // Allow preflight requests
